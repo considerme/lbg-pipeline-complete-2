0151dd119f7a0db9ff979c5961aff90ae0a74f42
SS <s.u@qa.com>
2024-02-09 11:18:34 +0000

    aa

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
         <DisplayBlock customstyle="field" label="VAT to pay: " value={vatToPay} />
         <PriceEntryField customstyle="field" label="Price incl VAT: " priceChanged={handleGrossPriceChange} price={grossPrice === 0.0 ? "" : grossPrice} />
       </div>
-      <img src="download.png">
+      <img src="download.png"></img>
     </div>
   );
 }
